65b40a432febe2a6cbcde739434fe3ce584d904a
Rintan <sawinee.rintan@gmail.com>
2023-04-10 15:17:52 +0700

    ASP.Net7

---
+++ BasicASPTutorial/Data/ApplicationBDContext.cs
@@ -0,0 +1,13 @@
+﻿using BasicASPTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicASPTutorial.Data
+{
+    public class ApplicationBDContext:DbContext
+    {
+        public ApplicationBDContext(DbContextOptions<ApplicationBDContext> options) :base(options) { 
+        
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
